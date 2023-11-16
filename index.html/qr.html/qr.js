4628fab10f04ce7da0ff91e1c4773924054c35d8
samasrikanthreddy <samasrikanthreddy49@gmail.com>
2023-11-17 00:22:24 +0530

    first commit

---
+++ index.html/qr.html/qr.js
@@ -0,0 +1,31 @@
+// Get a reference to the QR code container
+
+// Get a reference to the QR code container
+const qrcodeContainer = document.getElementById('qrcode');
+
+// Data for the QR code
+const data = 'https://www.example.com'; // Replace with your desired URL or data
+
+// Create a QR code instance
+const qrcode = new QRCode(qrcodeContainer, {
+    text: data,
+    width: 200,
+    height: 200,
+});
+
+
+
+// GET A REFERENCE TO THE QR CODEVCONTAINER
+//  const qrcodeContainer = document.getElementById('qrcode');
+
+//Dara for the QR code 
+//  const date = 'https://www.example.com';//Replace with your desired URL or data
+
+//Create a QR code instance
+
+
+// const qrcode = new QRCode(qrcodeContainer, {
+//   text:data,
+//   width:200,
+//  height:200,
+//  });
